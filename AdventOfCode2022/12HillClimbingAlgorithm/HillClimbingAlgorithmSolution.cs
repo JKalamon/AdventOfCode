560bfe9be6a626ecc20422f2ec899ef3c262d764
Jakub Kalamon <=>
2022-12-12 16:24:42 +0100

    day 12

--- AdventOfCode2022/12HillClimbingAlgorithm/HillClimbingAlgorithmSolution.cs
+++ AdventOfCode2022/12HillClimbingAlgorithm/HillClimbingAlgorithmSolution.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode2022;
+﻿using MoreLinq;
+
+namespace AdventOfCode2022;
 
 internal class HillClimbingAlgorithmSolution : IChallenge
 {
@@ -13,26 +15,77 @@ internal class HillClimbingAlgorithmSolution : IChallenge
 	public object SolvePart1()
 	{
 		var list = ParseInput();
-		return "ss";
+		Dijkstra(list);
+
+		return list.First(x => x.IsDestination).Distance;
 	}
 
 	public object? SolvePart2()
 	{
-		return null;
+		var least = int.MaxValue;
+		var list = ParseInput();
+		var i = 1;
+		foreach (var a in list.Where(x => x.Height == 0))
+		{
+			Console.WriteLine($"Processing {i} of {list.Where(x => x.Height == 0).Count()}");
+			list.ForEach(x =>
+			{
+				x.Distance = int.MaxValue;
+				x.Processed = false;
+			});
+
+			a.Distance = 0;
+			Dijkstra(list);
+			var dest = list.First(x => x.IsDestination);
+			if (dest.Distance < least)
+				least = dest.Distance;
+
+			i++;
+		}
+
+		return least;
 	}
 
-	private HillRecord[][] ParseInput()
+	void Dijkstra(IEnumerable<HillRecord> records)
 	{
-		var list = new HillRecord[Input.Length][];
+		while (records.Any(x => !x.Processed && x.Distance < int.MaxValue))
+		{
+			var recordToProcess = records.Where(x => !x.Processed).OrderBy(x => x.Distance).First();
+			foreach (var sib in GetSiblings(records, recordToProcess.X, recordToProcess.Y))
+			{
+				if (CanGo(recordToProcess, sib) && sib.Distance >= recordToProcess.Distance + 1)
+				{
+					sib.Distance = recordToProcess.Distance + 1;
+				}
+			}
+
+			recordToProcess.Processed = true;
+		}
+	}
+
+	private bool CanGo(HillRecord from, HillRecord to)
+	{
+		return from.Height + 1 >= to.Height;
+	}
+
+	private IEnumerable<HillRecord> GetSiblings(IEnumerable<HillRecord> records, int x, int y)
+	{
+		return records.Where(hr => (hr.Y == y && (hr.X == x - 1 || hr.X == x + 1) || (hr.X == x && (hr.Y == y - 1 || hr.Y == y + 1))));
+	}
+
+	private IEnumerable<HillRecord> ParseInput()
+	{
+		var list = new List<HillRecord>();
 		for (int i = 0; i < Input.Length; i++)
 		{
-			list[i] = new HillRecord[Input[i].Length];
 			for (int j = 0; j < Input[i].Length; j++)
 			{
-				list[i][j] = new HillRecord(Input[i][j], j, i);
-				if (list[i][j].IsCurrent)
+				var aa = new HillRecord(Input[i][j], j, i);
+				list.Add(aa);
+				if (Input[i][j] == 'S')
 				{
-					this.Current = list[i][j];
+					this.Current = aa;
+					aa.Distance = 0;
 				}
 			}
 		}
@@ -44,11 +97,17 @@ internal class HillClimbingAlgorithmSolution : IChallenge
 	{
 		private string priorityList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+		public HillRecord(int Height, int x, int y)
+		{
+			this.Height = Height;
+			this.X = x;
+			this.Y = y;
+		}
+
 		public HillRecord(char c, int x, int y)
 		{
 			if (c == 'S')
 			{
-				this.IsCurrent = true;
 				c = 'a';
 			}
 
@@ -69,8 +128,10 @@ internal class HillClimbingAlgorithmSolution : IChallenge
 
 		public int Y { get; init; }
 
-		public bool IsCurrent { get; set; }
-
 		public bool IsDestination { get; init; }
+
+		public int Distance { get; set; } = int.MaxValue;
+
+		public bool Processed { get; set; } = false;
 	}
 }
