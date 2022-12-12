b671234580edcbde8ef3c17d57f4fd98d91e6443
Jakub Kalamon <jakub.kalamon@gmail.com>
2022-12-12 08:29:24 +0100

    input parse for day 12

---
+++ AdventOfCode2022/11MonkeyInTheMiddle/MonkeyInTheMiddleSolution.cs
@@ -0,0 +1,45 @@
+﻿using MoreLinq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2022;
+
+internal class MonkeyInTheMiddleSolution : IChallenge
+{
+	public string Title => "--- Day 11: Monkey in the Middle ---";
+
+	public DateTime DateTime => new(2022, 12, 11);
+
+	private string[] Input = File.ReadAllLines("11MonkeyInTheMiddle/input.txt");
+
+	public object SolvePart1()
+	{
+		var counter = 0;
+		ParseInput();
+		return counter;
+	}
+
+	public object? SolvePart2()
+	{
+		return null;
+	}
+
+	private IEnumerable<Monkey> ParseInput()
+	{
+		var list = new List<Monkey>();
+		foreach (var lines in Input.Split(string.IsNullOrWhiteSpace))
+		{
+			var monkeyInput = lines.ToArray();
+			var id = Regex.Match(monkeyInput[0], @"Monkey ([0-9]+):").Groups[1].Value;
+			var startingItems = Regex.Match(monkeyInput[1], @"Starting items: (.+)").Groups[1].Value.Split(", ").Select(int.Parse);
+			var operation = Regex.Match(monkeyInput[2], @"Operation: (.+)").Groups[1].Value;
+			var test = int.Parse(Regex.Match(monkeyInput[3], @"Test: divisible by (.+)").Groups[1].Value);
+			var trueMonkey = int.Parse(Regex.Match(monkeyInput[4], @"If true: throw to monkey (.+)").Groups[1].Value);
+			var falseMonkey = int.Parse(Regex.Match(monkeyInput[5], @"If false: throw to monkey (.+)").Groups[1].Value);
+			//new Regex(@"Monkey \d:").Matches(monkeyInput[0]).
+		}
+
+		return list;
+	}
+}
+
+internal record Monkey(int Id, int[] Items, string Operation, int Test, int TrueMonkeyThrow, int FalseMonkeyThrow);
---
+++ AdventOfCode2022/12HillClimbingAlgorithm/HillClimbingAlgorithmSolution.cs
@@ -0,0 +1,76 @@
+﻿namespace AdventOfCode2022;
+
+internal class HillClimbingAlgorithmSolution : IChallenge
+{
+	public string Title => "--- Day 12: Hill Climbing Algorithm ---";
+
+	public DateTime DateTime => new(2022, 12, 12);
+
+	private string[] Input = File.ReadAllLines("12HillClimbingAlgorithm/input.txt");
+
+	private HillRecord Current = new HillRecord('S', 0, 0);
+
+	public object SolvePart1()
+	{
+		var list = ParseInput();
+		return "ss";
+	}
+
+	public object? SolvePart2()
+	{
+		return null;
+	}
+
+	private HillRecord[][] ParseInput()
+	{
+		var list = new HillRecord[Input.Length][];
+		for (int i = 0; i < Input.Length; i++)
+		{
+			list[i] = new HillRecord[Input[i].Length];
+			for (int j = 0; j < Input[i].Length; j++)
+			{
+				list[i][j] = new HillRecord(Input[i][j], j, i);
+				if (list[i][j].IsCurrent)
+				{
+					this.Current = list[i][j];
+				}
+			}
+		}
+
+		return list;
+	}
+
+	class HillRecord
+	{
+		private string priorityList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+		public HillRecord(char c, int x, int y)
+		{
+			if (c == 'S')
+			{
+				this.IsCurrent = true;
+				c = 'a';
+			}
+
+			if (c == 'E')
+			{
+				this.IsDestination = true;
+				c = 'z';
+			}
+
+			this.Height = priorityList.IndexOf(c);
+			this.X = x;
+			this.Y = y;
+		}
+
+		public int Height { get; init; }
+
+		public int X { get; init; }
+
+		public int Y { get; init; }
+
+		public bool IsCurrent { get; set; }
+
+		public bool IsDestination { get; init; }
+	}
+}
