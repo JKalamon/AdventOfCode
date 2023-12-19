d97de7d18465da8018661f4ee70307e1b25495c2
Jakub Kalamon <jakub.kalamon@gmail.com>
2023-12-19 01:28:12 +0100

    part 1

--- AdventOfCode2023/18LavaductLagoon/LavaductLagoonSolution.cs
+++ AdventOfCode2023/18LavaductLagoon/LavaductLagoonSolution.cs
@@ -1,4 +1,5 @@
 ﻿using MoreLinq;
+using System.Text;
 
 namespace AdventOfCode2023;
 
@@ -11,16 +12,23 @@ internal class LavaductLagoonSolution : ChallengeBase
 	public override object SolvePart1()
 	{
 		var maze = Dig(this.ParseInput());
-		var flood = FloodFill(maze, new Position(1, -1));
-		return maze.Concat(flood).Distinct().Count();
+		PrintToFile(maze);
+		return SpanFloodFill(maze, new Position(1, -1)) + maze.Sum(x => x.Length) - maze.Count();
 	}
+
+	//public override object SolvePart2()
+	//{
+	//	//var maze = Dig(this.ParseInput2());
+	//	//PrintToFile(maze);
+	//	return SpanFloodFill(maze, new Position(2, -1)) + maze.Sum(x => x.Length) - maze.Count();
+	//}
 
 	private IEnumerable<Line> Dig(IEnumerable<DigInstruction> instructions)
 	{
 		var position = new Position(0, 0);
 		foreach (var digInstruction in instructions)
 		{
-			var (direction, howFar, hexColor) = digInstruction;
+			var (direction, howFar) = digInstruction;
 
 			var newPosition = direction switch
 			{
@@ -38,13 +46,64 @@ internal class LavaductLagoonSolution : ChallengeBase
 
 	private long SpanFloodFill(IEnumerable<Line> loop, Position start)
 	{
-		var lx = start.X;
-		while (loop.Any(x => x.IsInLine(new Position(lx, start.Y))))
-			lx--;
-		var rx = start.X;
-		while (loop.Any(x => x.IsInLine(new Position(rx, start.Y))))
-			rx++;
-		
+		var fillLines = new List<Line>();
+		var queue = new Queue<Position>();
+		queue.Enqueue(start);
+
+		while (queue.Any())
+		{
+			var current = queue.Dequeue();
+			if (loop.Any(x => x.IsPointOnLine(current)))
+				continue;
+			var lx = FindLx(loop, current);
+			var rx = FindRx(loop, current);
+
+			var filledLine = new Line(new Position(lx, current.Y), new Position(rx, current.Y));
+			if (fillLines.Contains(filledLine))
+				continue;
+
+			fillLines.Add(filledLine);
+
+			////scan
+			loop.Where(x => x.DoLinesIntersect(Line.MaxHorizontal(current.Y + 1))).ForEach(x =>
+			{
+				if (x.MinX - 1 >= lx && x.MinX - 1 <= rx)
+					queue.Enqueue(new Position(x.MinX - 1, current.Y + 1));
+				if (x.MaxX + 1 <= rx && x.MaxX + 1 >= lx)
+					queue.Enqueue(new Position(x.MaxX + 1, current.Y + 1));
+			});
+
+			if (!queue.Any(x => x.Y == current.Y + 1) && !loop.Any(x => x.IsPointOnLine(new Position(lx, current.Y + 1))))
+			{
+				queue.Enqueue(new Position(lx, current.Y + 1));
+			}
+
+			loop.Where(x => x.DoLinesIntersect(Line.MaxHorizontal(current.Y - 1))).ForEach(x =>
+			{
+				if (x.MinX - 1 >= lx && x.MinX - 1 <= rx)
+					queue.Enqueue(new Position(x.MinX - 1, current.Y - 1));
+				if (x.MaxX + 1 <= rx && x.MaxX + 1 >= lx)
+					queue.Enqueue(new Position(x.MaxX + 1, current.Y - 1));
+			});
+
+			if (!queue.Any(x => x.Y == current.Y - 1) && !loop.Any(x => x.IsPointOnLine(new Position(lx, current.Y - 1))))
+			{
+				queue.Enqueue(new Position(lx, current.Y - 1));
+			}
+		}
+
+		//PrintToFile(loop.Concat(fillLines));
+		return fillLines.Sum(x => x.Length);
+	}
+
+	private int FindLx(IEnumerable<Line> loop, Position start)
+	{
+		return loop.Where(x => x.MaxX < start.X && x.DoLinesIntersect(Line.MaxHorizontal(start.Y))).OrderByDescending(x => x.MaxX).First().MaxX + 1;
+	}
+
+	private int FindRx(IEnumerable<Line> loop, Position start)
+	{
+		return loop.Where(x => x.MinX > start.X && x.DoLinesIntersect(Line.MaxHorizontal(start.Y))).OrderBy(x => x.MinX).First().MinX - 1;
 	}
 
 	private IEnumerable<Position> GetNeighbors(Position position)
@@ -55,54 +114,16 @@ internal class LavaductLagoonSolution : ChallengeBase
 		yield return new Position(position.X, position.Y - 1);
 	}
 
-	public override object SolvePart2()
+	private void PrintToFile(IEnumerable<Line> lines)
 	{
-		return "";
-		// var instructions = this.ParseInput().ToList();
-		// var loop = Dig(instructions).Distinct().ToList();
-		// var start = loop.MinBy(x => x.X).MinBy(x => x.Y).First();
-		// var floodFill = FloodFill(loop, start).ToList();
-		// var visited = new HashSet<Position>();
-		// var queue = new Queue<Position>();
-		// queue.Enqueue(start);
-		// while (queue.Any())
-		// {
-		// 	var current = queue.Dequeue();
-		// 	if (visited.Contains(current))
-		// 		continue;
-		// 	visited.Add(current);
-		// 	foreach (var neighbor in GetNeighbors(current))
-		// 	{
-		// 		if (visited.Contains(neighbor))
-		// 			continue;
-		// 		queue.Enqueue(neighbor);
-		// 	}
-		// }
+		var grid = Enumerable.Range(0, 1000).Select(x => new StringBuilder(string.Concat(Enumerable.Repeat('.', 1000)))).ToArray();
+		lines.SelectMany(x => x.Points).ForEach(x =>
+		{
+			grid[x.Y * -1 + 500].Remove(x.X, 1);
+			grid[x.Y * -1 + 500].Insert(x.X, '#');
+		});
 
-		// var unvisited = floodFill.Except(visited).ToList();
-		// var colors = unvisited.Select(x => instructions.First(y => y.Direction == Direction.Up && y.HowFar == 1 && y.HexColor == "#000000")).ToList();
-		// var colorIndex = 0;
-		// var color = colors[colorIndex];
-		// var result = 0;
-		// foreach (var position in unvisited)
-		// {
-		// 	if (position.X == 0 && position.Y == 0)
-		// 		continue;
-		// 	if (position.X == 0 && position.Y == 1)
-		// 		continue;
-		// 	if (position.X == 1 && position.Y == 0)
-		// 		continue;
-		// 	if (position.X == 1 && position.Y == 1)
-		// 		continue;
-		// 	result++;
-		// 	colorIndex++;
-		// 	if (colorIndex >= colors.Count)
-		// 		colorIndex = 0;
-		// 	color = colors[colorIndex];
-		// }
-
-		// return result;
-
+		File.WriteAllLines(this.InputPath.Replace(".txt", ".out.txt"), grid.Select(x => x.ToString()));
 	}
 
 	private IEnumerable<DigInstruction> ParseInput()
@@ -120,24 +141,133 @@ internal class LavaductLagoonSolution : ChallengeBase
 			};
 
 			var howFar = int.Parse(parts[1]);
-			var hexColor = parts[2].Replace("(#", "").Replace(")", "");
-			return new DigInstruction(direction, howFar, hexColor);
+			return new DigInstruction(direction, howFar);
 		});
 
-	private record DigInstruction(Direction Direction, int HowFar, string HexColor);
+	private IEnumerable<DigInstruction> ParseInput2()
+		=>
+		File.ReadAllLines(this.InputPath).Select(x =>
+		{
+			var parts = x.Split(' ');
+			var hexColor = parts[2].Replace("(#", "").Replace(")", "");
+
+			var howFar = Convert.ToInt32(hexColor[0..4], 16);
+			var dirChar = hexColor[5];
+			var direction = dirChar switch
+			{
+				'3' => Direction.Up,
+				'1' => Direction.Down,
+				'2' => Direction.Left,
+				'0' => Direction.Right,
+				_ => throw new Exception("Invalid direction")
+			};
+			return new DigInstruction(direction, howFar);
+		});
+
+	private int HexToDecimal(string hex)
+	{
+		string hexValues = "0123456789ABCDEF";
+		int decValue = 0;
+		for (int i = 0; i < hex.Length; i++)
+		{
+			char current = hex[i];
+			int next = hexValues.IndexOf(current);
+			decValue = decValue * 16 + next;
+		}
+
+		return decValue;
+	}
+
+	private record DigInstruction(Direction Direction, int HowFar);
 
 	private record Position(int X, int Y);
+	
+	private record Rectangle(Line Horizontal, Line Vertical);
 
 	private record Line(Position Start, Position End)
 	{
-		public IsInLine(Position position)
+		public long Length => MaxX - MinX + MaxY - MinY + 1;
+
+		public IEnumerable<Position> Points
 		{
-			if (Start.X == End.X)
-				return position.X == Start.X && position.Y >= Math.Min(Start.Y, End.Y) && position.Y <= Math.Max(Start.Y, End.Y);
-			if (Start.Y == End.Y)
-				return position.Y == Start.Y && position.X >= Math.Min(Start.X, End.X) && position.X <= Math.Max(Start.X, End.X);
-			throw new Exception("Invalid line");
-		}	
+			get
+			{
+				if (IsHorizontal)
+				{
+					for (int i = MinX; i <= MaxX; i++)
+						yield return new Position(i, Start.Y);
+				}
+				else if (IsVertical)
+				{
+					for (int i = MinY; i <= MaxY; i++)
+						yield return new Position(Start.X, i);
+				}
+				else
+				{
+					throw new Exception("Not a line");
+				}
+			}
+		}
+
+		public static Line MaxHorizontal(int Y) => new Line(new Position(int.MinValue, Y), new Position(int.MaxValue, Y));
+
+		public int MaxX => Math.Max(Start.X, End.X);
+
+		public int MinX => Math.Min(Start.X, End.X);
+
+		public int MaxY => Math.Max(Start.Y, End.Y);
+
+		public int MinY => Math.Min(Start.Y, End.Y);
+
+		public bool IsHorizontal => Start.Y == End.Y;
+
+		public bool IsVertical => Start.X == End.X;
+
+		public bool IsTouching(Position position)
+		{
+			if (IsVertical)
+				return Math.Abs(position.X - Start.X) == 1 && position.Y >= Math.Min(Start.Y, End.Y) && position.Y <= Math.Max(Start.Y, End.Y);
+			if (IsHorizontal)
+				return position.Y == Start.Y && (position.X == MinX - 1 || position.X == MinX + 1);
+
+			return false;
+		}
+
+		public bool DoLinesIntersect(Line other)
+		{
+			if (IsHorizontal && other.IsVertical)
+			{
+				return MinX <= other.MinX && other.MinX <= MaxX && other.MinY < MinY && MinY < other.MaxY;
+			}
+
+			if (IsVertical && other.IsHorizontal)
+			{
+				return other.MinX <= MinX && MinX <= other.MaxX && MinY <= other.MinY && other.MinY < MaxY;
+			}
+
+			if (IsHorizontal && other.IsHorizontal)
+			{
+				return MinY == other.MinY && ((MinX <= other.MinX && other.MinX <= MaxX) || (other.MinX <= MinX && MinX < other.MaxX));
+			}
+
+			return false; // not needed I guess both vertical
+		}
+
+		private bool IsOverlap(int start1, int end1, int start2, int end2)
+		{
+			return Math.Max(start1, start2) <= Math.Min(end1, end2);
+		}
+
+		public bool IsPointOnLine(Position point)
+		{
+			if (IsHorizontal)
+				return point.Y == Start.Y && point.X >= MinX && point.X <= MaxX;
+
+			if (IsVertical)
+				return point.X == Start.X && point.Y >= MinY && point.Y <= MaxY;
+
+			return false;
+		}
 	}
 
 	enum Direction
